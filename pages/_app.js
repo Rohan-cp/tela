335ed6a6ce1ec88ea84273167390f140d2d877df
Rohan-cp <rohancp9@icloud.com>
2023-05-01 12:37:48 +0530

    rollback web analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
-import '../styles/globals.css'
-import { Analytics } from '@vercel/analytics/react';
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return
+  return;
   <>
     <Component {...pageProps} />
-    <Analytics/>
-  </>
+  </>;
 }
