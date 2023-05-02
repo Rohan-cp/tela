2697173bf68012ad90e6d0122c44f26756a32721
Rohan-cp <rohancp9@icloud.com>
2023-05-02 12:53:51 +0530

    lint + ts migration + removed hp2 as source

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,54 @@
 import { useEffect, useState } from "react";
-import useSound from 'use-sound';
+import useSound from "use-sound";
 import AnimatedSpecialText from "../src/components/AnimatedSpecialText";
 
 export default function Home({
-    lookupWord,
-    source_kafka,
-    source_master,
-    source_lovecraft1,
-    source_lovecraft2,
-
-} ) {
-
-  const [play] = useSound('/typewriter-one-hit.mp3')
-  const [isReady, setIsReady] = useState(false)
-  const [showHighlight, setShowHighlight] = useState(false)
-  const [chosenWord, setChosenWord] = useState('not initialized')
-  const [chosenWordCounter, setChosenWordCounter] = useState(0)
-  const [currPhrase, setCurrPhrase] = useState([''])
-  const [tempValue, setTempValue] = useState('')
+  lookupWord,
+  source_kafka,
+  source_master,
+  source_lovecraft1,
+  source_lovecraft2,
+}) {
+  const [play] = useSound("/typewriter-one-hit.mp3");
+  const [isReady, setIsReady] = useState(false);
+  const [showHighlight, setShowHighlight] = useState(false);
+  const [chosenWord, setChosenWord] = useState("not initialized");
+  const [chosenWordCounter, setChosenWordCounter] = useState(0);
+  const [currPhrase, setCurrPhrase] = useState([""]);
+  const [tempValue, setTempValue] = useState("");
 
   useEffect(() => {
-    const data = JSON.parse(lookupWord)
-    const keys = Object.keys(data)
-    let currWord =  keys[Math.floor(Math.random() * 5000)]
+    const data = JSON.parse(lookupWord);
+    const keys = Object.keys(data);
+    let currWord = keys[Math.floor(Math.random() * 5000)];
     while (currWord.length <= 2) {
-      currWord = keys[Math.floor(Math.random() * 5000)]
+      currWord = keys[Math.floor(Math.random() * 5000)];
     }
     const sourceKafka = JSON.parse(source_kafka);
     const sourceMaster = JSON.parse(source_master);
     const sourceLovecraft1 = JSON.parse(source_lovecraft1);
     const sourceLovecraft2 = JSON.parse(source_lovecraft2);
-    const sourceCollection = [sourceKafka, sourceMaster, sourceLovecraft1, sourceLovecraft2]
-    const firstTextValue = data[currWord]
-    const instancesLength = firstTextValue.map(arr => arr.length || 0)
+    const sourceCollection = [
+      sourceKafka,
+      sourceMaster,
+      sourceLovecraft1,
+      sourceLovecraft2,
+    ];
+    const firstTextValue = data[currWord];
+    const instancesLength = firstTextValue.map((arr) => arr.length || 0);
 
-    const positiveInstanceIndicatorArr = instancesLength.map((n, index) => {
-      if (n !== 0) {
-        return index;
-      } else {
-        return -1;
-      }
-    }).filter(a => a !== -1);
+    const positiveInstanceIndicatorArr = instancesLength
+      .map((n, index) => {
+        if (n !== 0) {
+          return index;
+        } else {
+          return -1;
+        }
+      })
+      .filter((a) => a !== -1);
 
-    const n = positiveInstanceIndicatorArr.length;
+    // remove hp2 for now
+    const n = positiveInstanceIndicatorArr.length - 1;
     const randomNewIdx = Math.floor(Math.random() * n);
     const nextSourceLookupIdx = positiveInstanceIndicatorArr[randomNewIdx];
     const sentenceIdxs = firstTextValue[nextSourceLookupIdx];
@@ -51,24 +57,29 @@ export default function Home({
     const cleanSentence = sourceLookup[sentenceIdxs[0]].replace(/\n/g, " ");
     const cleanWords = cleanSentence.split(" ");
     setCurrPhrase(cleanWords);
-  }, [lookupWord, source_kafka, source_master, source_lovecraft1, source_lovecraft2])
-
+  }, [
+    lookupWord,
+    source_kafka,
+    source_master,
+    source_lovecraft1,
+    source_lovecraft2,
+  ]);
 
   useEffect(() => {
     function handleKeyDown(e) {
-      setIsReady(true)
+      setIsReady(true);
     }
 
-    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener("keydown", handleKeyDown);
 
     return function cleanup() {
-      document.removeEventListener('keydown', handleKeyDown);
-    }
+      document.removeEventListener("keydown", handleKeyDown);
+    };
   }, []);
 
   const handleChange = (e) => {
-    setTempValue(e.target.value)
-  }
+    setTempValue(e.target.value);
+  };
 
   useEffect(() => {
     const data = JSON.parse(lookupWord);
@@ -76,53 +87,67 @@ export default function Home({
     const sourceMaster = JSON.parse(source_master);
     const sourceLovecraft1 = JSON.parse(source_lovecraft1);
     const sourceLovecraft2 = JSON.parse(source_lovecraft2);
-    const sourceCollection = [sourceKafka, sourceMaster, sourceLovecraft1, sourceLovecraft2]
+    const sourceCollection = [
+      sourceKafka,
+      sourceMaster,
+      sourceLovecraft1,
+      sourceLovecraft2,
+    ];
 
     const hasKey = chosenWord in data;
     if (hasKey) {
-      const firstTextValue = data[chosenWord]
-      const instancesLength = firstTextValue.map(arr => arr.length || 0)
-      const positiveInstanceIndicatorArr = instancesLength.map((n, index) => {
-        if (n !== 0) {
-          return index;
-        } else {
-          return -1;
-        }
-      }).filter(a => a !== -1);
+      const firstTextValue = data[chosenWord];
+      const instancesLength = firstTextValue.map((arr) => arr.length || 0);
+      const positiveInstanceIndicatorArr = instancesLength
+        .map((n, index) => {
+          if (n !== 0) {
+            return index;
+          } else {
+            return -1;
+          }
+        })
+        .filter((a) => a !== -1);
 
       const n = positiveInstanceIndicatorArr.length;
       const randomNewIdx = Math.floor(Math.random() * n);
       const nextSourceLookupIdx = positiveInstanceIndicatorArr[randomNewIdx];
       const sentenceIdxs = firstTextValue[nextSourceLookupIdx];
       const m = sentenceIdxs.length;
-      const randomSentenceIdx = Math.floor(Math.random() * m)
-      const sourceLookup = sourceCollection[nextSourceLookupIdx]
+      const randomSentenceIdx = Math.floor(Math.random() * m);
+      const sourceLookup = sourceCollection[nextSourceLookupIdx];
 
       const selectedSentenceIdx = sentenceIdxs[randomSentenceIdx];
-      let words = sourceLookup[selectedSentenceIdx].split(" ")
+      let words = sourceLookup[selectedSentenceIdx].split(" ");
       if (words.length < 25) {
-        words = words.concat(sourceLookup[selectedSentenceIdx + 1])
+        words = words.concat(sourceLookup[selectedSentenceIdx + 1]);
       }
-      console.log("words after", words)
-      setShowHighlight(true)
+      console.log("words after", words);
+      setShowHighlight(true);
       setTimeout(() => {
         // console.log("chosenWord", chosenWord)
-        setShowHighlight(false)
+        setShowHighlight(false);
         words = words.join(" ").replace(/\n/g, " ").split(" ");
         setCurrPhrase(words);
       }, 2000);
     }
-  }, [chosenWord, chosenWordCounter, lookupWord, source_kafka, source_master, source_lovecraft1, source_lovecraft2])
+  }, [
+    chosenWord,
+    chosenWordCounter,
+    lookupWord,
+    source_kafka,
+    source_master,
+    source_lovecraft1,
+    source_lovecraft2,
+  ]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    play()
+    play();
     setChosenWord(tempValue);
-    setChosenWordCounter(currCounter => currCounter + 1)
+    setChosenWordCounter((currCounter) => currCounter + 1);
     setTempValue("");
-  }
-
+  };
 
   // console.log("currPhrase", currPhrase)
   if (isReady) {
@@ -132,33 +157,51 @@ export default function Home({
         <div id="stars2"></div>
         <div id="stars3"></div>
         <div className="experience-wrapper">
-        <AnimatedSpecialText text={currPhrase} chosenWord={chosenWord} showHighlight={showHighlight} />
+          <AnimatedSpecialText
+            text={currPhrase}
+            chosenWord={chosenWord}
+            showHighlight={showHighlight}
+          />
           <form onSubmit={handleSubmit}>
-            <input value={tempValue} type="text" onChange={handleChange} className="prompt-word-input" />
+            <input
+              value={tempValue}
+              type="text"
+              onChange={handleChange}
+              className="prompt-word-input"
+            />
           </form>
         </div>
       </main>
-    )
+    );
   }
 
   return (
     <main className="flex min-h-screen flex-col">
-        <div id="stars"></div>
-        <div id="stars2"></div>
-        <div id="stars3"></div>
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
       <div className="wrapper">
-        <div style={{display: "flex", flexDirection: "column"}}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
           <span className="title">TELA</span>
-          <span className="subtitle" >by rohan prashanth</span>
+          <span className="subtitle">by rohan prashanth</span>
         </div>
-        <span>Move between passages by typing a word you see on the screen and hit enter or pave your own path.</span>
-        <span>Hit any key to start.</span>
-        <span>Tela is an interactive non-linear text explorer that lets you travel static text in a uniquely-disconnected path. By providing connections
-          between different passages (from even different works) based on keywords and randomness, new narratives and interpretations from the original linear stories may emerge.
+        <span>
+          Move between passages by typing a word you see on the screen and hit
+          enter or pave your own path.
         </span>
-        <span>Inspired by my exploration into experiencing literature in new ways.</span>
+        <span>Hit any key to start.</span>
+        <span>
+          Tela is an interactive non-linear text explorer that lets you travel
+          static text in a uniquely-disconnected path. By providing connections
+          between different passages (from even different works) based on
+          keywords and randomness, new narratives and interpretations from the
+          original linear stories may emerge.
+        </span>
+        <span>
+          Inspired by my exploration into experiencing literature in new ways.
+        </span>
         <div>
-        <span>This experience includes the following texts:</span>
+          <span>This experience includes the following texts:</span>
           <ul>
             <li>The Master & Margarita by Mikhail Bulgakov</li>
             <li>Kafka on the Shore by Haruki Murakami</li>
@@ -167,18 +210,17 @@ export default function Home({
         </div>
       </div>
     </main>
-  )
+  );
 }
 
 export async function getStaticProps() {
   // Call an external API endpoint to get posts.
   // You can use any data fetching library
-  const lookup = await import('../data/lookup.json');
-  const source_kafka_json = await import('../data/kafka-shore.json');
-  const source_master_json = await import('../data/master.json');
-  const source_lovecraft1_json = await import('../data/lovecraft1.json');
-  const source_lovecraft2_json = await import('../data/lovecraft2.json');
-
+  const lookup = await import("../data/lookup.json");
+  const source_kafka_json = await import("../data/kafka-shore.json");
+  const source_master_json = await import("../data/master.json");
+  const source_lovecraft1_json = await import("../data/lovecraft1.json");
+  const source_lovecraft2_json = await import("../data/lovecraft2.json");
 
   const source_kafka = JSON.stringify(source_kafka_json);
   const source_master = JSON.stringify(source_master_json);
@@ -197,5 +239,5 @@ export async function getStaticProps() {
       source_lovecraft1,
       source_lovecraft2,
     },
-  }
+  };
 }
---
+++ src/components/AnimatedSpecialText.js
@@ -0,0 +1,50 @@
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+
+const AnimatedSpecialText = ({ text, chosenWord, showHighlight }) => {
+  const [show, setShow] = useState([""]);
+  const words = text
+    .join(" ")
+    .replace(/\n/g, " ")
+    .split(" ")
+    .filter((word) => word !== "");
+
+  useEffect(() => {
+    const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+    setShow(
+      words.map((word) => {
+        const isHighlighted = word.replace(regex, "") === chosenWord;
+        return isHighlighted && showHighlight ? "highlighted" : "usual";
+      })
+    );
+  }, [showHighlight, chosenWord, words]);
+
+  return (
+    <motion.div
+      style={{
+        overflow: "hidden",
+        display: "flex",
+        fontSize: "1.3rem",
+        flexWrap: "wrap",
+      }}
+    >
+      {words.map((word, index) => (
+        <motion.span
+          style={{ marginRight: "5px" }}
+          key={index}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{
+            duration: 1 + index / 10,
+            delay: 0,
+          }}
+          className={show[index]}
+        >
+          {word.trim()}
+        </motion.span>
+      ))}
+    </motion.div>
+  );
+};
+
+export default AnimatedSpecialText;
